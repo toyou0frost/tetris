1d996e906c062389dc6e7e517f1fc7451eb69d2c
butdory <67633798+butdory@users.noreply.github.com>
2021-01-02 02:23:08 +0900

    Update 방프.js

--- tetris/방프.js
+++ tetris/방프.js
@@ -1,84 +1,18 @@
 var _5lineI = 1;
 var _5lineColor = "aqua";
-var defalutColor = "black"
+var defalutColor = "rgb(0, 0, 0)";
 
 setInterval(function () {
     var _now5line = document.getElementById("_" + _5lineI + "-5");
     var Next = _5lineI + 1;
     var _5lineNext = document.getElementById("_" + Next + "-5");
-    alert("i"+_5lineI);
-    alert("N"+Next);
-    if (_5lineI != 19) {
+    var tmp = getComputedStyle(_5lineNext).backgroundColor;
+    if (tmp == defalutColor) {
         _5lineI++;
-    }
-    else {
-        _5lineI = 1;
-    }
-    if (_now5line.style.backgroundColor == defalutColor) {
         _5lineNext.style.backgroundColor = _5lineColor;
-        alert("a");
         _now5line.style.backgroundColor = defalutColor;
-        alert("b");
+    }
+    else{
+        _5lineI = 1;
     }
 }, 500)
-
-/*setInterval(function () {
-    var _now5line = document.getElementById("_" + _5lineI + "-5");
-    var Next = _5lineI + 1;
-    var _5lineNext = document.getElementById("_" + Next + "-5");
-    if (_5lineI != 18) {
-        _5lineI++;
-    }
-    else {
-        alert("b");
-        _5lineI = 1;
-    }
-    _now5line.style.backgroundColor = defalutColor;
-    if (_now5line.style.backgroundColor == defalutColor) {
-        _5lineNext.style.backgroundColor = _5lineColor;
-    }
-    else{
-        alert("b");
-    }
-}, 1000)
-
-setInterval(function () {
-    var _now5line = document.getElementById("_" + _5lineI + "-5");
-    var Next = _5lineI + 1;
-    var _5lineNext = document.getElementById("_" + Next + "-5");
-    if (_5lineI != 19) {
-        _5lineI++;
-    }
-    else {
-        alert("c");
-        _5lineI = 1;
-    }
-    _now5line.style.backgroundColor = defalutColor;
-    if (_now5line.style.backgroundColor == defalutColor) {
-        _5lineNext.style.backgroundColor = _5lineColor;
-    }
-    else{
-        alert("c");
-    }
-}, 1000)
-
-setInterval(function () {
-    var _now5line = document.getElementById("_" + _5lineI + "-5");
-    var Next = _5lineI + 1;
-    var _5lineNext = document.getElementById("_" + Next + "-5");
-    if (_5lineI != 20) {
-        _5lineI++;
-    }
-    else {
-        alert("d");
-        _5lineI = 1;
-    }
-    _now5line.style.backgroundColor = defalutColor;
-    if (_now5line.style.backgroundColor == defalutColor) {
-        _5lineNext.style.backgroundColor = _5lineColor;
-    }
-    else{
-        alert("d");
-    }
-}, 1000)
-*/
