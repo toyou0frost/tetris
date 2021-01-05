48b4ee0a21ff9fbfbfba33a7ea05246b80a7d26e
butdory <67633798+butdory@users.noreply.github.com>
2021-01-05 14:20:08 +0900

    버그3 픽스

--- tetris/방프.js
+++ tetris/방프.js
@@ -1,4 +1,4 @@
-var _5lineI = 1;
+var _5lineI = 0;
 var _5lineColor = "rgb(0, 255, 255)";
 var defalutColor = "rgb(0, 0, 0)";
 
@@ -8,15 +8,20 @@ setInterval(function () {
     for(var i = 1; i <= 10; i++){
         var _now5line = document.getElementById("_" + _5lineI + "-"+i);
         var _5lineNext = document.getElementById("_" + Next + "-"+i);
+        if(_now5line == "_19-5"){
+            alert();
+            _5lineI = 0;
+        }
         var tmp = getComputedStyle(_5lineNext).backgroundColor;// 버그 1
         var tmp1 = getComputedStyle(_now5line).backgroundColor;// 버그 2
-        if (tmp == defalutColor && _now5line != "_20-5" && tmp1 == _5lineColor) { // 버그 3
-            _5lineNext.style.backgroundColor = _5lineColor;
-            _now5line.style.backgroundColor = defalutColor;
-        }
-        else{
-            _5lineI = 1;
+        if (tmp == defalutColor) { 
+            if(tmp1 == _5lineColor){
+                _5lineNext.style.backgroundColor = _5lineColor;
+                _now5line.style.backgroundColor = defalutColor;
+            }
         }
     }
     _5lineI++;
-}, 500)
+}, 500) 
+
+
