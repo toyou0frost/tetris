ff37bfca7f9964ef24a801db03ba24dd728f9907
butdory <67633798+butdory@users.noreply.github.com>
2021-01-06 18:30:12 +0900

    Update 방프.js

--- tetris/방프.js
+++ tetris/방프.js
@@ -1,5 +1,4 @@
 var _lineI = 0;
-var RdBlock = 0;
 var defalutColor = "rgb(0, 0, 0)";
 var _iblock = "rgb(0, 255, 255)";
 var _jblock = "rgb(0, 0, 255)";
@@ -9,10 +8,25 @@ var _sblock = "rgb(152, 251, 152)";
 var _tblock = "rgb(128, 0, 128)";
 var _zblock = "rgb(255, 0, 0)";
 var _nowBlockColor;
+var BlockType = [0, 1, 2, 3, 4, 5, 6];
+var RdBlock = BlockType;
+var blockCreateI = 0;
 
 onload = function(){
+    shuffleArray(BlockType);
     BlockCreate(RdBlock);  
 }
+
+const shuffleArray = array => {
+    for (let i = 0; i < array.length; i++) {
+        let j = Math.floor(Math.random() * (i + 1));
+        const x = array[i];
+        array[i] = array[j];
+        array[j] = x;
+    }
+    return array;
+};
+
 
 
 setInterval(function BlockFall() {
@@ -24,6 +38,9 @@ setInterval(function BlockFall() {
         var _nowLineColor = getComputedStyle(_nowLine).backgroundColor;
         if(_nowLine.id == "_20-"+i){
             _lineI = 0;
+            if(blockCreateI == 6){
+                blockCreateI = 0;
+            }
             BlockCreate(RdBlock);
         }
         if (_nowLineNextColor == defalutColor) { 
@@ -37,12 +54,14 @@ setInterval(function BlockFall() {
                 alert("gameover");
             }
             _lineI = 0;
+            if(blockCreateI == 6){
+                blockCreateI = 0;
+            }
             BlockCreate(RdBlock);
         }
     }
     _lineI++;
 }, 50) 
-
 
 function BlockCreate(RdBlock){
     var _0_4 = document.getElementById("_0-4");
@@ -52,58 +71,73 @@ function BlockCreate(RdBlock){
     var _1_4 = document.getElementById("_1-4");
     var _1_5 = document.getElementById("_1-5");
     var _1_6 = document.getElementById("_1-6");
-    switch(RdBlock){
-        case 0: 
-            _0_4.style.backgroundColor = _iblock; 
-            _0_5.style.backgroundColor = _iblock;
-            _0_6.style.backgroundColor = _iblock;
-            _0_7.style.backgroundColor = _iblock;
-            _nowBlockColor = _iblock;
-            BlockFall();
-            break;
-        case 1:
-            _0_4.style.backgroundColor = _jblock; 
-            _1_4.style.backgroundColor = _jblock;
-            _1_5.style.backgroundColor = _jblock;
-            _1_6.style.backgroundColor = _jblock;
-            _nowBlockColor = _iblock;
-            break;
-        case 2:
-            _0_6.style.backgroundColor = _lblock;
-            _1_4.style.backgroundColor = _lblock; 
-            _1_5.style.backgroundColor = _lblock;
-            _1_6.style.backgroundColor = _lblock;
-            _nowBlockColor = _lblock;
-            break;
-        case 3:
-            _0_5.style.backgroundColor = _oblock; 
-            _0_6.style.backgroundColor = _oblock;
-            _1_5.style.backgroundColor = _oblock;
-            _1_6.style.backgroundColor = _oblock;
-            _nowBlockColor = _oblock;
-            break;
-        case 4:
-            _0_5.style.backgroundColor = _sblock; 
-            _0_6.style.backgroundColor = _sblock;
-            _1_4.style.backgroundColor = _sblock;
-            _1_5.style.backgroundColor = _sblock;
-            _nowBlockColor = _sblock;
-            break;
-        case 5:
-            _0_5.style.backgroundColor = _tblock;
-            _1_4.style.backgroundColor = _tblock; 
-            _1_5.style.backgroundColor = _tblock;
-            _1_6.style.backgroundColor = _tblock;
-            _nowBlockColor = _tblock;
-            break;
-        case 6:
-            _0_4.style.backgroundColor = _zblock; 
-            _0_5.style.backgroundColor = _zblock;
-            _1_5.style.backgroundColor = _zblock;
-            _1_6.style.backgroundColor = _zblock;
-            _nowBlockColor = _zblock;
-            break;
-    }
+    for(blockCreateI; blockCreateI < 7;){
+        switch(RdBlock[blockCreateI]){
+            case 0: 
+                _0_4.style.backgroundColor = _iblock; 
+                _0_5.style.backgroundColor = _iblock;
+                _0_6.style.backgroundColor = _iblock;
+                _0_7.style.backgroundColor = _iblock;
+                _nowBlockColor = _iblock;
+                blockCreateI++
+                BlockFall();
+                break;
+            case 1:
+                _0_4.style.backgroundColor = _jblock; 
+                _1_4.style.backgroundColor = _jblock;
+                _1_5.style.backgroundColor = _jblock;
+                _1_6.style.backgroundColor = _jblock;
+                _nowBlockColor = _iblock;
+                blockCreateI++
+                BlockFall();
+                break;
+            case 2:
+                _0_6.style.backgroundColor = _lblock;
+                _1_4.style.backgroundColor = _lblock; 
+                _1_5.style.backgroundColor = _lblock;
+                _1_6.style.backgroundColor = _lblock;
+                _nowBlockColor = _lblock;
+                blockCreateI++
+                BlockFall();
+                break;
+            case 3:
+                _0_5.style.backgroundColor = _oblock; 
+                _0_6.style.backgroundColor = _oblock;
+                _1_5.style.backgroundColor = _oblock;
+                _1_6.style.backgroundColor = _oblock;
+                _nowBlockColor = _oblock;
+                blockCreateI++
+                BlockFall();
+                break;
+            case 4:
+                _0_5.style.backgroundColor = _sblock; 
+                _0_6.style.backgroundColor = _sblock;
+                _1_4.style.backgroundColor = _sblock;
+                _1_5.style.backgroundColor = _sblock;
+                _nowBlockColor = _sblock;
+                blockCreateI++
+                BlockFall();
+                break;
+            case 5:
+                _0_5.style.backgroundColor = _tblock;
+                _1_4.style.backgroundColor = _tblock; 
+                _1_5.style.backgroundColor = _tblock;
+                _1_6.style.backgroundColor = _tblock;
+                _nowBlockColor = _tblock;
+                blockCreateI++
+                BlockFall();
+                break;
+            case 6:
+                _0_4.style.backgroundColor = _zblock; 
+                _0_5.style.backgroundColor = _zblock;
+                _1_5.style.backgroundColor = _zblock;
+                _1_6.style.backgroundColor = _zblock;
+                _nowBlockColor = _zblock;
+                blockCreateI++
+                BlockFall();
+                break;
+        }
+    }  
 }
 
 document.addEventListener('keydown', (event) => {
