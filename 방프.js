1385455b95c6fb427742afb1ec2e9f7269cdd986
butdory <butdory04@gmail.com>
2021-01-06 17:54:56 +0900

    asd

--- 방프.js
+++ 방프.js
@@ -1,25 +1,165 @@
-var _5lineI = 0;
-var _5lineColor = "rgb(0, 255, 255)";
+var _lineI = 0;
+var RdBlock = 0;
 var defalutColor = "rgb(0, 0, 0)";
+var _iblock = "rgb(0, 255, 255)";
+var _jblock = "rgb(0, 0, 255)";
+var _lblock = "rgb(255, 140, 0)";
+var _oblock = "rgb(255, 215, 0)";
+var _sblock = "rgb(152, 251, 152)";
+var _tblock = "rgb(128, 0, 128)";
+var _zblock = "rgb(255, 0, 0)";
+var _nowBlockColor;
 
-setInterval(function () {
-    var Next = _5lineI + 1;
+onload = function(){
+    BlockCreate(RdBlock);  
+}
+
+
+setInterval(function BlockFall() {
+    var Next = _lineI + 1;
     for(var i = 1; i <= 10; i++){
-        var _now5line = document.getElementById("_" + _5lineI + "-"+i);
-        var _5lineNext = document.getElementById("_" + Next + "-"+i);
-        var tmp = getComputedStyle(_5lineNext).backgroundColor;
-        var tmp1 = getComputedStyle(_now5line).backgroundColor;
-        if(_now5line.id == "_20-"+i){
-            _5lineI = 0;
+        var _nowLine = document.getElementById("_" + _lineI + "-"+i);
+        var _nowLineNext = document.getElementById("_" + Next + "-"+i);
+        var _nowLineNextColor = getComputedStyle(_nowLineNext).backgroundColor;
+        var _nowLineColor = getComputedStyle(_nowLine).backgroundColor;
+        if(_nowLine.id == "_20-"+i){
+            _lineI = 0;
+            BlockCreate(RdBlock);
         }
-        if (tmp == defalutColor) { 
-            if(tmp1 == _5lineColor){
-                _5lineNext.style.backgroundColor = _5lineColor;
-                _now5line.style.backgroundColor = defalutColor;
+        if (_nowLineNextColor == defalutColor) { 
+            if(_nowLineColor == _nowBlockColor){
+                _nowLineNext.style.backgroundColor = _nowBlockColor;
+                _nowLine.style.backgroundColor = defalutColor;
+            }
+        }
+        else{
+            if(_nowLine.id == "_0-4" || _nowLine.id == "_0-5" || _nowLine.id == "_0-6" || _nowLine.id == "_0-7" ){
+                alert("gameover");
+            }
+            _lineI = 0;
+            BlockCreate(RdBlock);
+        }
+    }
+    _lineI++;
+}, 50) 
+
+
+function BlockCreate(RdBlock){
+    var _0_4 = document.getElementById("_0-4");
+    var _0_5 = document.getElementById("_0-5");
+    var _0_6 = document.getElementById("_0-6");
+    var _0_7 = document.getElementById("_0-7");
+    var _1_4 = document.getElementById("_1-4");
+    var _1_5 = document.getElementById("_1-5");
+    var _1_6 = document.getElementById("_1-6");
+    switch(RdBlock){
+        case 0: 
+            _0_4.style.backgroundColor = _iblock; 
+            _0_5.style.backgroundColor = _iblock;
+            _0_6.style.backgroundColor = _iblock;
+            _0_7.style.backgroundColor = _iblock;
+            _nowBlockColor = _iblock;
+            BlockFall();
+            break;
+        case 1:
+            _0_4.style.backgroundColor = _jblock; 
+            _1_4.style.backgroundColor = _jblock;
+            _1_5.style.backgroundColor = _jblock;
+            _1_6.style.backgroundColor = _jblock;
+            _nowBlockColor = _iblock;
+            break;
+        case 2:
+            _0_6.style.backgroundColor = _lblock;
+            _1_4.style.backgroundColor = _lblock; 
+            _1_5.style.backgroundColor = _lblock;
+            _1_6.style.backgroundColor = _lblock;
+            _nowBlockColor = _lblock;
+            break;
+        case 3:
+            _0_5.style.backgroundColor = _oblock; 
+            _0_6.style.backgroundColor = _oblock;
+            _1_5.style.backgroundColor = _oblock;
+            _1_6.style.backgroundColor = _oblock;
+            _nowBlockColor = _oblock;
+            break;
+        case 4:
+            _0_5.style.backgroundColor = _sblock; 
+            _0_6.style.backgroundColor = _sblock;
+            _1_4.style.backgroundColor = _sblock;
+            _1_5.style.backgroundColor = _sblock;
+            _nowBlockColor = _sblock;
+            break;
+        case 5:
+            _0_5.style.backgroundColor = _tblock;
+            _1_4.style.backgroundColor = _tblock; 
+            _1_5.style.backgroundColor = _tblock;
+            _1_6.style.backgroundColor = _tblock;
+            _nowBlockColor = _tblock;
+            break;
+        case 6:
+            _0_4.style.backgroundColor = _zblock; 
+            _0_5.style.backgroundColor = _zblock;
+            _1_5.style.backgroundColor = _zblock;
+            _1_6.style.backgroundColor = _zblock;
+            _nowBlockColor = _zblock;
+            break;
+    }
+}
+
+document.addEventListener('keydown', (event) => {
+    const keyCode = event.key;
+    if(keyCode == "ArrowLeft"){
+        var cnt = 0
+        for(var i = 0; i < 20; i++){
+            for(var j = 1; j <= 10; j++){
+                var blockCheck = document.getElementById("_"+i+"-"+j);
+                var blockCheckColor = getComputedStyle(blockCheck).backgroundColor
+                var leftJ = j - 1;
+                var nowBlockLeft = document.getElementById("_"+i+"-"+leftJ);
+                if(blockCheckColor == _nowBlockColor){
+                    cnt++;
+                    if(nowBlockLeft.id == "_"+i+"-0" || cnt == 5){
+                        break;
+                    }
+                    nowBlockLeft.style.backgroundColor = _nowBlockColor;
+                    blockCheck.style.backgroundColor = defalutColor;
+                }
             }
         }
     }
-    _5lineI++;
-}, 100) 
-
-
+    else if(keyCode == "ArrowRight"){
+        var cnt = 0;
+        for(var i = 0; i < 20; i++){
+            for(var j = 10; j >= 0; j--){
+                var blockCheck = document.getElementById("_"+i+"-"+j);
+                var blockCheckColor = getComputedStyle(blockCheck).backgroundColor
+                var leftJ = j + 1;
+                var nowBlockLeft = document.getElementById("_"+i+"-"+leftJ);
+                if(blockCheckColor == _nowBlockColor){
+                    cnt++;
+                    if(nowBlockLeft.id == "_"+i+"-11" || cnt == 5){
+                        break;
+                    }
+                    nowBlockLeft.style.backgroundColor = _nowBlockColor;
+                    blockCheck.style.backgroundColor = defalutColor;
+                }
+            }
+        }
+    }
+    /*
+    else if(keyCode == "ArrowDown"){
+        for(var i = 20; i > 0; i--){
+            for(var j = 0; j <= 10; j++){
+                var blockCheck = document.getElementById("_"+i+"-"+j);
+                var blockCheckColor = getComputedStyle(blockCheck).backgroundColor
+                var lefti = i + 1;
+                if(blockCheckColor == _nowBlockColor){
+                    var nowBlockLeft = document.getElementById("_"+lefti+"-"+j);
+                    nowBlockLeft.style.backgroundColor = _nowBlockColor;
+                    blockCheck.style.backgroundColor = defalutColor;
+                    Next++;
+                }
+            }
+        }
+    }*/
+})
